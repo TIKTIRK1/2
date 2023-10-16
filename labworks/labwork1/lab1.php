ada5ea3708d23788a27e178970ef1e536a823619
Матюшонок Марк <mmdmark1@gmail.com>
2023-10-16 11:06:04 +0500

    0.6

--- labworks/labwork1/lab1.php
+++ labworks/labwork1/lab1.php
@@ -1,13 +1,98 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta http-equiv="Content-Type" Content="text/html; charset=utf-8">
     <title>Лабораторная работа №1</title>
+    <style type="text/css">
+        H4{
+            font-size: 120%;
+            font-family: Verdana, Arial, Helvetica, sans-serif;
+            color: #FF0000;
+            text-align: left;
+        }
+    </style>
+
+    <style type="text/css">
+        H2{
+            font-size: 160%;
+            font-family: Verdana, Arial, Helvetica, sans-serif;
+            color: #00FF00;
+            text-align: center;
+        }
+    </style>
+
+    <style type="text/css">
+        H3{
+            font-size: 200%;
+            font-family: Verdana, Arial, Helvetica, sans-serif;
+            color: #0000FF;
+            text-align: right;
+        }
+    </style>
 </head>
 <body>
     <h1>
         Лабораторная работа №1
     </h1>
-    <hr>  
+    <hr> 
+    <p align="center">
+    <?php
+    echo "Hello!!!"; 
+    ?>
+    <p align="center">
+    <?php echo "Hello!!!";  ?>
+    <p><hr><br>
+
+    <p align="center">
+    <?php echo "Hello!!!"; ?>
+    &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+    <?php echo "Hello!!!";?>  
+    <p><hr><br>
+
+    <p align="center">
+    <?php echo "Hello!!!";  ?>
+    <p align="center">
+        <span style="color:red; font-size:2em">
+            <?php 
+                echo "Hello!!!";  
+            ?>
+        </span>
+    <p>
+    <hr>
+    <br>
+    <p align="center">
+        <span style="color:green; font-size:3em">
+            <?php 
+                echo "Hello!!!"; 
+            ?>
+            &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+            <?php 
+                echo "Hello!!!";
+            ?> 
+        </span> 
+    <p>
+    <hr>
+    <br>
+    <p align="center">
+        <span style="color:blue; font-size:2em">   
+        <?php 
+            echo "Hello!!!";  
+        ?>
+        </span>
+    <p>
+    <?php 
+        echo "<H4>";
+        echo "Hello world!";
+        echo "</H4>"; 
+        
+        echo "<H2>";
+        echo "Hello world!";
+        echo "</H2>";
+
+        echo "<H3>";
+        echo "Hello world!";
+        echo "</H3>";
+    ?>
+
 </body>
 </html>
-<?php ?>
--- labworks/labwork2/lab2.php
+++ labworks/labwork2/lab2.php
@@ -1,49 +1,54 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta http-equiv="Content-Type" Content="text/html; charset=utf-8">
-    <title>Стили</title>
-
-    <style type="text/css">
-        H1{
-            font-size: 120%;
-            font-family: Verdana, Arial, Helvetica, sans-serif;
-            color: #FF0000;
-            text-align: left;
-        }
-    </style>
-
-    <style type="text/css">
-        H2{
-            font-size: 160%;
-            font-family: Verdana, Arial, Helvetica, sans-serif;
-            color: #00FF00;
-            text-align: center;
-        }
-    </style>
-
-    <style type="text/css">
-        H3{
-            font-size: 200%;
-            font-family: Verdana, Arial, Helvetica, sans-serif;
-            color: #0000FF;
-            text-align: right;
-        }
-    </style>
+    <title>Лабораторная работа №2</title>
 </head>
 <body>
-    <?php 
-        echo "<H1>";
-        echo "Hello world!";
-        echo "</H1>"; 
-        
-        echo "<H2>";
-        echo "Hello world!";
-        echo "</H2>";
+<h1>
+    Лабораторная работа №2
+</h1>
+    <hr> 
+<?PHP
+    $Zima = "-1";
+    $zima = -1; 
+    $_Zima = -1.0;
 
-        echo "<H3>";
-        echo "Hello world!";
-        echo "</H3>";
-    ?>
+    $type_Zima = gettype($Zima);
+    $type_zima = gettype($zima); 
+    $type__Zima = gettype($_Zima);
+    echo "Значение переменнной \$Zima = $Zima. Это тип данных \"$type_Zima\"<br />"; 
+    echo "Значение переменнной \$zima = $zima. Это тип данных \"$type_zima\"<br />"; 
+    echo "Значение переменнной \$_Zima = $_Zima. Это тип данных \"$type__Zima\"<br />";
+    echo "План \"Тень-2\" начнется через два-три месяца<br />";
+?>
+<?PHP
+    $int_max=PHP_INT_MAX; 
+    echo "<br />Max Integer: $int_max";
+    $int_min=PHP_INT_MIN;
+    echo "<br />Min integer: $int_min";
+    $int_max32 = (2**32)/2-1;
+    echo "<br />Max integer: $int_max32";
+?>
+<?PHP
+    echo 'это простая строка';
+    echo "<br>","<br>";
+    echo 'Также вы можете вставлять в строки символ новой строки<br> 
+    таким образом,поскольку это нормально'; 
+    echo "<br>","<br>";
+    // Выведет. Однажды Арнольд сказал: "I'll be back"
+    echo 'Однажды он сказал: "I\'ll be back"';
+    echo "<br>","<br>";
+    // Выведет: Вы удалили С:\*.*? 
+    echo 'Вы удалили C:\*.*?';
+    echo "<br>","<br>";
+    // Выведет: вы удалили C:\*.*2 
+    echo 'Вы удалили С:\*.*?';
+    echo "<br>","<br>";
+    // Выведет. Это не вставит: \n новую строку
+    echo 'это не вставит:\n в новую строку';
+    echo "<br>","<br>";
+    // Выведет: Переменные Sexpand также Seither не подставляются
+    echo 'Переменные $expand также $either не подставляются';
+?>
 </body>
 </html>
