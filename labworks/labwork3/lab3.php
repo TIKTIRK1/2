0232f2559a3d76e63627346dc1a67ddf464b37d4
Матюшонок Марк <mmdmark1@gmail.com>
2023-10-19 14:21:00 +0500

    0.8

--- labworks/labwork3/lab3.php
+++ labworks/labwork3/lab3.php
@@ -2,48 +2,10 @@
 <html>
 <head>
     <meta http-equiv="Content-Type" Content="text/html; charset=utf-8">
-    <title>Стили</title>
+    <title>Лабораторная работа №3</title>
 
-    <style type="text/css">
-        H1{
-            font-size: 120%;
-            font-family: Verdana, Arial, Helvetica, sans-serif;
-            color: #FF0000;
-            text-align: left;
-        }
-    </style>
-
-    <style type="text/css">
-        H2{
-            font-size: 160%;
-            font-family: Verdana, Arial, Helvetica, sans-serif;
-            color: #00FF00;
-            text-align: center;
-        }
-    </style>
-
-    <style type="text/css">
-        H3{
-            font-size: 200%;
-            font-family: Verdana, Arial, Helvetica, sans-serif;
-            color: #0000FF;
-            text-align: right;
-        }
-    </style>
 </head>
 <body>
-    <?php 
-        echo "<H1>";
-        echo "Hello world!";
-        echo "</H1>"; 
-        
-        echo "<H2>";
-        echo "Hello world!";
-        echo "</H2>";
-
-        echo "<H3>";
-        echo "Hello world!";
-        echo "</H3>";
-    ?>
+ 
 </body>
 </html>
